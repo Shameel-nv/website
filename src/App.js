230aef176ccfd1670a0274b5e2d81142937f7ee5
shameel-nv <you@muhammedshameelnvnv@gmail.com>
2024-04-30 12:16:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import react from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <center>
+        <div className="heading">
+          <h1>
+            ABOUT ME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MY
+            WORK&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;CONTACT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </h1>
+        </div>
+        <div className="h2">
+          <h2>
+            I&nbsp;&nbsp;&nbsp;SAW&nbsp;&nbsp;&nbsp;THE&nbsp;&nbsp;&nbsp;SIGN
+          </h2>
+        </div>
+        <div className="para">
+          <p>
+            and it opened up my eyes.One of my favourite things to do is to
+            capture the american nostalgia of signage.They tellus where we're
+            going or where we've been.They express a mood or saying can even
+            just be a shape.But it's the vintage, neon,and big light bulbed
+            signs that say so much more.They aren't just your average square
+            shapes with letters on it. some actual thought and planning went
+            into the making of the sign.Oh and the lost are of advertising with
+            neon signs that display motion is always a fun find! Such a lost art
+            in my opinion.LED just hurts my eyes....doesn't
+          </p>
+        </div>
+        <img
+          className="image"
+          src="http://www.lovethispic.com/uploaded_images/335482-This-Is-The-Sign-You-ve-Been-Looking-For.jpg"
+        ></img>
+
+        <div className="image-container">
+          <div>
+            <img
+              className="img1"
+              src="http://www.lovethispic.com/uploaded_images/335482-This-Is-The-Sign-You-ve-Been-Looking-For.jpg"
+            ></img>
+            <img
+              className="img1"
+              src="http://www.lovethispic.com/uploaded_images/335482-This-Is-The-Sign-You-ve-Been-Looking-For.jpg"
+            ></img>
+            <img
+              className="img1"
+              src="http://www.lovethispic.com/uploaded_images/335482-This-Is-The-Sign-You-ve-Been-Looking-For.jpg"
+            ></img>
+          </div>
+          <div>
+            <img
+              className="img1"
+              src="http://www.lovethispic.com/uploaded_images/335482-This-Is-The-Sign-You-ve-Been-Looking-For.jpg"
+            ></img>
+            <img
+              className="img1"
+              src="http://www.lovethispic.com/uploaded_images/335482-This-Is-The-Sign-You-ve-Been-Looking-For.jpg"
+            ></img>
+            <img
+              className="img1"
+              src="http://www.lovethispic.com/uploaded_images/335482-This-Is-The-Sign-You-ve-Been-Looking-For.jpg"
+            ></img>
+          </div>
+        </div>
+      </center>
+    </div>
+  );
+}
+export default App;
